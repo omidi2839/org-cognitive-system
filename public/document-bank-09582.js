fa0968d9ccd91f8640fca425053fe563c7886c60
omidi2839 <hosseinportal@gmail.com>
2026-09-08 14:05:25 +0330

    Add files via upload

--- public/document-bank-09582.js
+++ public/document-bank-09582.js
@@ -1,5 +1,5 @@
 (()=>{
-window.__DOCUMENT_BANK_BUILD__='0.9.5.8.2';
+window.__DOCUMENT_BANK_BUILD__='0.9.5.8.3';
 const FA='۰۱۲۳۴۵۶۷۸۹';
 const toFa=v=>String(v??'').replace(/\d/g,d=>FA[d]);
 const esc=s=>String(s??'').replace(/[&<>"']/g,m=>({'&':'&amp;','<':'&lt;','>':'&gt;','"':'&quot;',"'":'&#39;'}[m]));
