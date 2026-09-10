2b29119125417e8d6399d05864ab1ee471653a8f
omidi2839 <hosseinportal@gmail.com>
2026-09-10 16:37:11 +0330

    Delete api/auth-proxy.js

--- api/auth-proxy.js
+++
@@ -1,7 +0,0 @@
-import handler from './index.js';
-import { requireAuthenticated } from '../src/infrastructure/authSession.js';
-
-export default async function authenticatedApiProxy(req,res){
-  if(!requireAuthenticated(req,res)) return;
-  return handler(req,res);
-}
