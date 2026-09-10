92de455c9c2b8e82a58fa5cc9000404170d5b4dd
omidi2839 <hosseinportal@gmail.com>
2026-09-10 16:27:40 +0330

    Add files via upload

--- public/document-meeting-command-09582.js
+++ public/document-meeting-command-09582.js
@@ -1,5 +1,5 @@
 (()=>{
-window.__DOCUMENT_MEETING_COMMAND_BUILD__='0.9.8.9';
+window.__DOCUMENT_MEETING_COMMAND_BUILD__='0.9.8.9.1';
 const ORG='ORG:SYN-001',FA='۰۱۲۳۴۵۶۷۸۹';
 const toFa=v=>String(v??'').replace(/\d/g,d=>FA[d]);
 const esc=s=>String(s??'').replace(/[&<>"']/g,m=>({'&':'&amp;','<':'&lt;','>':'&gt;','"':'&quot;',"'":'&#39;'}[m]));
