15b361b9de6a0dc88b68dfee75d7f7ffe78306c0
omidi2839 <hosseinportal@gmail.com>
2026-09-12 10:16:39 +0330

    Add files via upload

--- public/legal-relation-contract-0990.js
+++ public/legal-relation-contract-0990.js
@@ -1,5 +1,5 @@
 (()=>{
-window.__LEGAL_RELATION_CONTRACT_BUILD__='0.9.9.0.18';
+window.__LEGAL_RELATION_CONTRACT_BUILD__='0.9.9.0.20';
 const esc=s=>String(s??'').replace(/[&<>"']/g,m=>({'&':'&amp;','<':'&lt;','>':'&gt;','"':'&quot;',"'":'&#39;'}[m]));
 const FA='۰۱۲۳۴۵۶۷۸۹',toFa=v=>String(v??'').replace(/\d/g,d=>FA[d]);
 
